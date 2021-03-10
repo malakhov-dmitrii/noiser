250347eb30511c438569f7c898b47b9bfa16110f
Dmitriy Malakhov <mitia2022@gmail.com>
2021-03-10 11:36:49 +0300

    added analytics

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,25 @@
 import { Box, Container, createMuiTheme, IconButton, Paper, ThemeProvider } from '@material-ui/core';
 import { VolumeOff, VolumeUp } from '@material-ui/icons';
-import React from 'react';
+import React, { useEffect } from 'react';
 import { useDispatch, useSelector } from 'react-redux';
 import './App.css';
 import Home from './pages/Home';
 import useTheme from './shared/hooks/useTheme';
 import { RootState } from './store';
 import { toggle } from './store/features/player';
+import ReactGA from 'react-ga';
+
+ReactGA.initialize('G-4EYB120GY2');
+
+// <!-- Global site tag (gtag.js) - Google Analytics -->
+// <script async src="https://www.googletagmanager.com/gtag/js?id=G-4EYB120GY2"></script>
+// <script>
+//   window.dataLayer = window.dataLayer || [];
+//   function gtag(){dataLayer.push(arguments);}
+//   gtag('js', new Date());
+
+//   gtag('config', 'G-4EYB120GY2');
+// </script>
 
 const baseTheme = createMuiTheme();
 const defaultTheme = createMuiTheme({
@@ -38,6 +51,10 @@ function App() {
   const { theme } = useTheme();
   const { isPlaying } = useSelector((state: RootState) => state.player);
   const dispatch = useDispatch();
+
+  useEffect(() => {
+    ReactGA.pageview(window.location.pathname + window.location.search);
+  }, []);
 
   return (
     <ThemeProvider theme={theme === 'dark' ? darkTheme : defaultTheme}>
