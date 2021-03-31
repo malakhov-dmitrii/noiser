8d73ab1c6c3968257ae4709abe6e3430ea58ed90
Dmitriy Malakhov <mitia2022@gmail.com>
2021-03-31 12:59:48 +0300

    ph lauch

--- src/App.tsx
+++ src/App.tsx
@@ -103,6 +103,22 @@ function App() {
             <Button variant="text" startIcon={<Email />} href="mailto:mitia2022@gmail.com" target="_blank" rel="noopener">
               Email author
             </Button>
+
+            <Box px={3}>
+              <a
+                rel="noopener noreferrer"
+                href="https://www.producthunt.com/posts/noizer?utm_source=badge-featured&utm_medium=badge&utm_souce=badge-noizer"
+                target="_blank"
+              >
+                <img
+                  src="https://api.producthunt.com/widgets/embed-image/v1/featured.svg?post_id=290204&theme=light"
+                  alt="Noizer - Ambient sounds. Like Noisli, but free | Product Hunt"
+                  style={{ width: '250px', height: '54px;' }}
+                  width="250"
+                  height="54"
+                />
+              </a>
+            </Box>
           </Box>
         </Box>
       </ThemeProvider>
